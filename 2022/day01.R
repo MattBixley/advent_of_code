43d3c7c0ebfb76d02e41f55b891e0ae3d862fdda
mattbixley <mattbixley72@gmail.com>
2022-12-01 21:40:44 +1300

    d1

--- 2022/day01.R
+++ 2022/day01.R
@@ -1,16 +1,26 @@
 library(tidyverse)
+library(plyr)
 
-input <- read_lines("2022/day01_input.txt") 
-|>
-  str_split()
+input <- readLines('2022/day01_input.txt')
 
+seps <- which(input == "")
+
+start <- c(1, seps[-length(seps)] + 1)
+end <- seps
+
+inputs <- purrr::map2(start, end, ~as.numeric(input[.x:(.y - 1)]))
+
+# Part 1 
+all_sums <- sapply(inputs, sum)
+out1 <- max(all_sums)
+
+# Part 2 
+out2 <- sort(all_sums, decreasing = TRUE)[1:3] |> 
+  sum()
+
+# Results
 # part 1
-input |>
-out1 <- 
 cat("Puzzle 1 solution:", out1, fill = TRUE)
 
-
 # part 2
-
-out2 <- 
 cat("Puzzle 2 solution:", out2, fill = TRUE)
