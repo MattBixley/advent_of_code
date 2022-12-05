0468421781080ebfd3adf2d1ee8b57089fee3853
mattbixley <mattbixley72@gmail.com>
2022-12-06 12:30:14 +1300

    back on proper tonight

---
+++ 2022/day05.R
@@ -0,0 +1,42 @@
+# Read in file
+input <- readLines("2022/day05_input.txt")
+# Figure out where the instructions begin
+w <- grep("move",input)[1]
+m <- max(as.integer(el(strsplit(input[w-2],""))),na.rm=TRUE)
+# Read the crate stacks as a fixed width table
+mat <- read.fwf(file="2022/day05_input.txt",widths=rep(4,m),n=(w-3))
+# Get rid of non integer characters and clean up
+mat <- apply(mat,2,\(x)gsub("[][ ]","",x))
+stacks <- list()
+for(i in 1:ncol(mat)){
+  stacks[[i]] <- mat[,i]
+  stacks[[i]] <- stacks[[i]][stacks[[i]]!=""]
+}
+# Duplicate the initial setup for part 2
+stacks2 <- stacks
+
+# Parse the instructions
+# parse.group is defined in a separate file as i tend to use it a lot
+inst <- parse.group("^move (?<n>[0-9]+) from (?<from>[0-9]+) to (?<to>[0-9]+)$", input[w:length(input)])
+inst <- as.data.frame(apply(inst,2,as.integer))
+
+# Part 1
+for(i in 1:nrow(inst)){
+  substack <- stacks[[inst$from[i]]][1:inst$n[i]]
+  stacks[[inst$from[i]]] <- stacks[[inst$from[i]]][-(1:inst$n[i])]
+  stacks[[inst$to[i]]] <- c(rev(substack),stacks[[inst$to[i]]])
+}
+cat(sapply(stacks,el),sep="")
+#FRDSQRRCD
+
+# Part 2
+for(i in 1:nrow(inst)){
+  substack <- stacks2[[inst$from[i]]][1:inst$n[i]]
+  stacks2[[inst$from[i]]] <- stacks2[[inst$from[i]]][-(1:inst$n[i])]
+  stacks2[[inst$to[i]]] <- c(substack,stacks2[[inst$to[i]]])
+}
+cat(sapply(stacks2,el),sep="")
+#HRFTQVWNN
+
+
+
--- utils.R
+++ utils.R
@@ -44,3 +44,14 @@ decimal_to_n_digit_binary <- function(num, n) {
 sort_chars <- function(word) {
   paste(sort(str_split(word, "")[[1]]), collapse = "")
 }
+
+parse.group <- function(regex,input) { #old faithful (from regexpr help file)
+  result <- regexpr(regex, input, perl=TRUE)
+  m <- do.call(rbind, lapply(seq_along(input), function(i) {
+    if(result[i] == -1) return("")
+    st <- attr(result, "capture.start")[i, ]
+    substring(input[i], st, st + attr(result, "capture.length")[i, ] - 1)
+  }))
+  colnames(m) <- attr(result, "capture.names")
+  m
+}
