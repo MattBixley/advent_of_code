675215389b310d7d307860e5eadf9b88f3a3d9b5
mattbixley <mattbixley72@gmail.com>
2024-11-22 13:59:23 +1300

    2015 8

--- 2015/day08_solution.R
+++ 2015/day08_solution.R
@@ -2,7 +2,16 @@
 library(tidyverse)
 
 # Read input data
-input <- read_lines("input.txt")
+input <- read_lines("2015/input08.txt")
+#
 
-# Your code here
+# Part 1
+
+sum(purrr::map_int(input, nchar)) -
+  sum(purrr::map_int(input, ~nchar(eval(parse(text = .x)), type = "bytes")))
+
+# Part 2
+
+sum(purrr::map_int(stringi::stri_escape_unicode(input), nchar) + 2) -
+  sum(purrr::map_int(input, nchar))
 
