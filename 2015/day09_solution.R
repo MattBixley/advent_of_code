ab7311b6bea1f198cbb8f1ae067e69c626d6ac7a
mattbixley <mattbixley72@gmail.com>
2024-11-27 15:03:39 +1300

    some more 2015

--- 2015/day09_solution.R
+++ 2015/day09_solution.R
@@ -2,7 +2,38 @@
 library(tidyverse)
 
 # Read input data
-input <- read_lines("input.txt")
+input <- read_lines("2015/input09.txt")
 
-# Your code here
+# Part 1
 
+input_tbl <- tibble(input) %>%
+  separate(input, c("destinations", "distance"), " = ", convert = TRUE) %>%
+  separate(destinations, c("to", "from"), " to ")
+
+all_paths <- input_tbl %>%
+  select(-distance) %>%
+  unlist() %>%
+  unique() %>%
+  combinat::permn()
+
+distances <- bind_rows(
+  input_tbl,
+  input_tbl %>% rename(from = to, to = from)
+)
+
+calc_distance <- function(x) {
+  tibble(
+    from = x[-length(x)],
+    to = x[-1]
+  ) %>%
+    left_join(distances, by = c("from", "to")) %>%
+    summarise(sum = sum(distance)) %>%
+    pull(sum)
+}
+
+purrr::map_int(all_paths, calc_distance) %>%
+  min()
+
+# Part 2
+purrr::map_int(all_paths, calc_distance) %>%
+  max()
--- 2015/day10_solution.R
+++ 2015/day10_solution.R
@@ -2,7 +2,29 @@
 library(tidyverse)
 
 # Read input data
-input <- read_lines("input.txt")
+input <- "1113122113"
 
-# Your code here
+look_and_say <- function(x) {
+  x <- strsplit(x, "")[[1]]
+  x <- rle(x)
+  x <- unlist(x)
+  x <- matrix(x, nrow = 2, byrow = TRUE)
+  x <- as.numeric(x)
+  paste0(x, collapse = "")
+}
+
+for (i in seq_len(40)) {
+  input <- look_and_say(input)
+}
+
+nchar(input)
+
+# Part 2
+
+input <- "1113122113"
+
+for (i in seq_len(50)) {
+  input <- look_and_say(input)
+}
+nchar(input)
 
