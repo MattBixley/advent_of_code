28679c5b4036f02a574df1525e6531d907e6f68d
mattbixley <mattbixley72@gmail.com>
2024-12-02 20:54:48 +1300

    missed a file

--- 2024/day2_solution.R
+++ 2024/day2_solution.R
@@ -1,11 +1,40 @@
 library(tidyverse)
 
 # Read the input file
-input <- read_lines('2024/day2_input.txt')
+read_list <- function(path, sep = "", type = identity) {
+  lines <- readLines(path)
+  res <- strsplit(lines, sep)
+  res <- lapply(res, type)
+  res
+}
+
+input <- read_list("2024/day2_input.txt", " ", as.integer)
+
+
 
 # Part 1
-# Your solution code for Part 1 here
+safe <- function(x) {
+  diffs <- diff(x)
+  cond1 <- all(diffs > 0) || all(diffs < 0)
+  cond2 <- all(abs(diffs) >= 1) && all(abs(diffs) <= 3)
+  cond1 && cond2
+}
+
+vapply(input, safe, logical(1)) |>
+  sum()
 
 # Part 2
-# Your solution code for Part 2 here
+skip_safe <- function(x) {
+  is_safe <- FALSE
+  for (i in seq_along(x)) {
+    if (safe(x[-i])) {
+      is_safe <- TRUE
+      break
+    }
+  }
+  is_safe
+}
+
+vapply(input, skip_safe, logical(1)) |>
+  sum()
 
